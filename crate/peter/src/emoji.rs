0dc05187102c15336ade37ff62126b98b1b7cf2e
Fenhl <fenhl@fenhl.net>
2021-09-30 17:31:51 +0000

    emoji: Update for changed twemoji repo structure

--- crate/peter/src/emoji.rs
+++ crate/peter/src/emoji.rs
@@ -8,42 +8,31 @@ use {
         fs,
         io,
         mem,
-        str::FromStr
+        str::FromStr,
     },
+    derive_more::From,
     once_cell::sync::Lazy,
     regex::Regex,
-    serenity::model::prelude::*
+    serenity::model::prelude::*,
 };
 
 static FILENAME_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new("^([0-9a-f]{1,6}(?:-[0-9a-f]{1,6})*)\\.svg$").expect("failed to compile twemoji filename regex"));
 static CUSTOM_EMOJI_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new("^<:[0-9A-Z_a-z]{2,}:[0-9]+>").expect("failed to compile custom emoji regex"));
 
 /// An error that can occur while parsing emoji from a message.
-#[derive(Debug)]
+#[derive(Debug, From)]
 pub enum Error {
     /// An error occurred while decoding a filename.
     FilenameDecode(OsString),
     /// A `std::io::Error` occurred.
-    Io(io::Error)
-}
-
-impl From<OsString> for Error {
-    fn from(e: OsString) -> Error {
-        Error::FilenameDecode(e)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Error {
-        Error::Io(e)
-    }
+    Io(io::Error),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             Error::FilenameDecode(ref s) => write!(f, "failed to read twemoji filename: {:?}", s),
-            Error::Io(ref e) => write!(f, "io error while building emoji db: {}", e)
+            Error::Io(ref e) => write!(f, "io error while building emoji db: {}", e),
         }
     }
 }
@@ -55,14 +44,14 @@ impl std::error::Error for Error {}
 /// Note that the `animated` field of yielded values is bogus and should not be relied upon.
 pub struct Iter {
     text: String,
-    emoji: Vec<String>
+    emoji: Vec<String>,
 }
 
 impl Iter {
     /// Create an iterator over all emoji in the given text.
     pub fn new(text: String) -> Result<Iter, Error> {
         let mut emoji = BTreeSet::default();
-        for entry in fs::read_dir("/opt/git/github.com/twitter/twemoji/master/2/svg")? {
+        for entry in fs::read_dir("/opt/git/github.com/twitter/twemoji/master/assets/svg")? {
             let file_name = entry?.file_name().into_string()?;
             if let Some(capture) = FILENAME_REGEX.captures(&file_name).and_then(|captures| captures.get(1)) {
                 // convert the filename encoding the emoji (e.g. 1f3f3-fe0f-200d-1f308.svg) to the emoji itself (e.g. 🏳️‍🌈)
@@ -71,7 +60,7 @@ impl Iter {
         }
         Ok(Iter {
             text,
-            emoji: emoji.into_iter().collect()
+            emoji: emoji.into_iter().collect(),
         })
     }
 }
@@ -87,16 +76,16 @@ impl Iterator for Iter {
                 let capture = captures.get(0).expect("failed to capture match object").as_str();
                 if let Ok(emoji_id) = EmojiIdentifier::from_str(capture) {
                     self.text = text[capture.len()..].to_owned();
-                    break Some(emoji_id.into());
+                    break Some(emoji_id.into())
                 }
             }
             if let Some(emoji) = self.emoji.iter().rev().filter(|&emoji| text.starts_with(emoji)).next() { // longest emoji first
                 self.text = text[emoji.len()..].to_owned();
-                break Some(ReactionType::Unicode(emoji.to_owned()));
+                break Some(ReactionType::Unicode(emoji.to_owned()))
             }
             match text.char_indices().nth(1) {
-                Some((idx, _)) => { text = &text[idx..]; }
-                None => { break None; }
+                Some((idx, _)) => text = &text[idx..],
+                None => break None,
             }
         }
     }
@@ -104,6 +93,6 @@ impl Iterator for Iter {
 
 /// Given a number in `0..26`, returns the regional indicator emoji corresponding to the letter in this position of the alphabet.
 pub fn nth_letter(n: u8) -> ReactionType {
-    if n >= 26 { panic!("letter not in range"); }
+    if n >= 26 { panic!("letter not in range") }
     ReactionType::Unicode(::std::char::from_u32('🇦' as u32 + n as u32).expect("failed to create regional indicator").to_string())
 }
