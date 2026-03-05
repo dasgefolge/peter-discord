1ebd3ec91401ab731645e2b54f24a642a4a7426e
Fenhl <fenhl@fenhl.net>
2026-03-05 10:39:32 +0000

    ipc: Add add_channel_access function

--- crate/peter/src/ipc.rs
+++ crate/peter/src/ipc.rs
@@ -12,6 +12,16 @@ serenity_utils::ipc! {
 
     const PORT: u16 = 18807;
 
+    /// Creates a permission override for a given channel and user, allowing them to view the channel and send messages.
+    async fn add_channel_access(ctx: &Context, channel: ChannelId, user: UserId) -> Result<(), String> {
+        channel.create_permission(ctx, PermissionOverwrite {
+            allow: Permissions::VIEW_CHANNEL | Permissions::SEND_MESSAGES,
+            deny: Permissions::default(),
+            kind: PermissionOverwriteType::Member(user),
+        }).await.map_err(|e| format!("failed to create permission overwrite: {e}"))?;
+        Ok(())
+    }
+
     /// Adds the given role to the given user. No-op if the user already has the role.
     async fn add_role(ctx: &Context, user: UserId, role: RoleId) -> Result<(), String> {
         let roles = iter::once(role).chain(GEFOLGE.member(ctx, user).await.map_err(|e| format!("failed to get member data: {e}"))?.roles.into_iter());
